cd8e74d8738784cb37cbc66c579edbca6689c693
Andjela <andjela.lazarevic@telenor.rs>
2018-05-20 11:58:14 +0200

    PlanActivity PROPO

--- app/src/main/java/com/diatom/kasicomat/PlanActivity.java
+++ app/src/main/java/com/diatom/kasicomat/PlanActivity.java
@@ -1,11 +1,20 @@
 package com.diatom.kasicomat;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
 
 import com.jjoe64.graphview.GraphView;
 
 public class PlanActivity extends AppCompatActivity {
+
+    final Context context = this;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,31 @@ public class PlanActivity extends AppCompatActivity {
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        button = (Button) findViewById(R.id.btnIzmeniPlan);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+                alertDialogBuilder.setTitle("Predlog izmene plana za štednju");
+                alertDialogBuilder
+                        .setMessage("BLABLA!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                PlanActivity.this.finish();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
